7e70339d641a020d7b97448027b4fb9293c3553d
shinjiezumi <shinjiezumi.work@gmail.com>
2023-01-06 21:54:37 +0900

    add set

---
+++ python_learning/12_set.py
@@ -0,0 +1,29 @@
+# set
+my_set = {1, 2, 3}
+print(type(my_set))  # <class 'set'>
+print(my_set)  # {1, 2, 3}
+
+# 空のset
+my_set = set()
+
+# 配列から変換
+my_list = [1, 2, 3]
+my_set = set(my_list)
+print(my_set)  # {1, 2, 3}
+
+# 重複した値は削除される
+my_list = [1, 1, 2, 3, 3]
+my_set = set(my_list)
+print(my_set)  # {1, 2, 3}
+
+# add で追加
+my_set.add(4)
+
+# remove で削除
+my_set.remove(1)
+
+print(my_set)  # {2, 3, 4}
+
+# in や len も使える
+print(3 in my_set)  # True
+print(len(my_set))  # 3
