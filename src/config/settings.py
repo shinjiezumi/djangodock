561042db4ab3b9184d65f37f990632880b4fc4f8
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-04 15:05:16 +0900

    add diary app

--- src/config/settings.py
+++ src/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'diary.apps.DiaryConfig',
     'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- src/config/urls.py
+++ src/config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls'))
+    path('myapp/', include('myapp.urls')),
+    path('diary/', include('diary.urls'))
 ]
