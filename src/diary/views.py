10ed54a4d06469c0071997120c882cf5ddec016e
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-06 22:32:33 +0900

    create update diary page

--- src/diary/views.py
+++ src/diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
 from .forms import DayCreateForm
@@ -24,6 +24,23 @@ def add(request):
     print(timezone.now())
     print(timezone.localtime(timezone.now()))
     # 通常遷移時や入力に誤りがあればページを表示
+    context = {
+        'form': form
+    }
+    return render(request, 'diary/day_form.html', context)
+
+
+def update(request, pk):
+    # pkを元に日記を取得
+    day = get_object_or_404(Day, pk=pk)
+
+    # フォームに反映
+    form = DayCreateForm(request.POST or None, instance=day)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+
     context = {
         'form': form
     }
