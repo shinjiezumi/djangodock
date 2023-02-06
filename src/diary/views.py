a891da5b52946bcc11f99b32fabf908f2e23d673
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-06 22:44:11 +0900

    create detail page

--- src/diary/views.py
+++ src/diary/views.py
@@ -59,3 +59,13 @@ def delete(request, pk):
         'day': day
     }
     return render(request, 'diary/day_confirm_delete.html', context)
+
+
+def detail(request, pk):
+    # pkを元に日記を取得
+    day = get_object_or_404(Day, pk=pk)
+
+    context = {
+        'day': day
+    }
+    return render(request, 'diary/day_detail.html', context)
