c4b7633daeafa9777413884d7870df7c0b5cb7ab
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-07 23:23:12 +0900

    add class-based-view sample

--- src/diary/views.py
+++ src/diary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.utils import timezone
+from django.views import generic
 
 from .forms import DayCreateForm
 from .models import Day
@@ -9,7 +11,7 @@ def index(request):
     context = {
         'day_list': Day.objects.all()
     }
-    return render(request, 'diary/list.html', context)
+    return render(request, 'diary/day_list.html', context)
 
 
 def add(request):
@@ -69,3 +71,29 @@ def detail(request, pk):
         'day': day
     }
     return render(request, 'diary/day_detail.html', context)
+
+
+# クラスビュー版
+class IndexView(generic.ListView):
+    model = Day
+
+
+class AddView(generic.CreateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:class_view_index')
+
+
+class UpdateView(generic.UpdateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:class_view_index')
+
+
+class DeleteView(generic.DeleteView):
+    model = Day
+    success_url = reverse_lazy('diary:class_view_index')
+
+
+class DetailView(generic.DetailView):
+    model = Day
