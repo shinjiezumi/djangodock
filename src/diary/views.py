aa1f4d9321042b5a5b4ecc58dda4bf9b91e2acf3
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-08 20:17:14 +0900

    allow editing only for admin user

--- src/diary/views.py
+++ src/diary/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -79,19 +80,19 @@ class IndexView(generic.ListView):
     paginate_by = 3
 
 
-class AddView(generic.CreateView):
+class AddView(LoginRequiredMixin, generic.CreateView):
     model = Day
     form_class = DayCreateForm
     success_url = reverse_lazy('diary:class_view_index')
 
 
-class UpdateView(generic.UpdateView):
+class UpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Day
     form_class = DayCreateForm
     success_url = reverse_lazy('diary:class_view_index')
 
 
-class DeleteView(generic.DeleteView):
+class DeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Day
     success_url = reverse_lazy('diary:class_view_index')
 
