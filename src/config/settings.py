3cf91ebffbde9374b48c96644d6806a8b422ae7b
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-09 23:02:20 +0900

    add employee app

--- src/config/settings.py
+++ src/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'employee.apps.EmployeeConfig',
     'diary.apps.DiaryConfig',
     'myapp.apps.MyappConfig',
     'django.contrib.admin',
