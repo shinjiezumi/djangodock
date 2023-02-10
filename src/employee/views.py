8ad0d958e67d456e3e1fc0ed10d59cea2ba933f2
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-10 23:09:29 +0900

    add employee and department model

--- src/employee/views.py
+++ src/employee/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 
+from .models import Employee
 
-class IndexView(generic.TemplateView):
-    template_name = 'employee/employee_list.html'
+
+class IndexView(generic.ListView):
+    model = Employee
