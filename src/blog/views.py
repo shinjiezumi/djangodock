006589aa7b61ec4661cc423be95d880ea08da239
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-16 00:07:53 +0900

    add category filter

--- src/blog/views.py
+++ src/blog/views.py
@@ -16,3 +16,23 @@ class IndexView(generic.ListView):
                 Q(title__icontains=keyword) | Q(text__icontains=keyword)  # ブログ名、本文の部分一致
             )
         return queryset
+
+
+class CategoryView(generic.ListView):
+    model = Post
+    paginate_by = 10
+
+    """ 書き方1 """
+    #     def get_queryset(self):
+    #         category = get_object_or_404(Category, pk=self.kwargs['pk'])  # 指定されたカテゴリオブジェクトを取得
+    #         queryset = Post.objects.order_by('-created_at').filter(category=category)  # 絞り込み
+    #         return queryset
+
+    """
+    書き方2
+    """
+
+    def get_queryset(self):
+        category_pk = self.kwargs['pk']  # 指定されたカテゴリID取得
+        queryset = Post.objects.order_by('-created_at').filter(category__pk=category_pk)  # 絞り込み
+        return queryset
