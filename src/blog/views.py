9068b45d60e14be6a42266a1b38ec321c13b5af8
shinjiezumi <shinjiezumi.work@gmail.com>
2023-02-15 23:55:54 +0900

    add search form

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views import generic
 
 from .models import Post
@@ -7,4 +8,10 @@ class IndexView(generic.ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.order_by('-created_at')
+        queryset = Post.objects.order_by('-created_at')
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(
+                Q(title__icontains=keyword) | Q(text__icontains=keyword)  # ブログ名、本文の部分一致
+            )
+        return queryset
